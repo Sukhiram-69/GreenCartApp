9762c32e9ca47f57201930d4e70610953dfbe3b1
Sukhiram <bonusahu1999@gmail.com>
2025-06-03 21:50:29 +0530

    Update App structure and context; add UI components and environment config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
 
 const App = () => {
   const isSellerpath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
   return (
     <div>
       {isSellerpath ? null : <Navbar />}
-
+      {showUserLogin ? <Login /> : null}
+      <Toaster />
       <div
         className={`${isSellerpath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
       >
@@ -15,6 +21,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerpath && <Footer />}
     </div>
   );
 };
